cb2f1558b84f7d279bc49e2b98d1da3b1feb8355
Jimin Kim <0305jimin@gmail.com>
2025-10-14 18:45:55 +0900

    feat: connecting to signin API

--- community-frontend/assets/js/signin.js
+++ community-frontend/assets/js/signin.js
@@ -4,21 +4,34 @@ const form = document.querySelector('form.signinForm');
 const btn = form.querySelector('button');
 
 form.addEventListener('submit', async (e) => {
-      e.preventDefault();
-    console.log('button clicked');
-});
+      try {
+            console.log('button clicked');
+            e.preventDefault();
+            btn.disabled = true;
+            const formData = new FormData(form);
+            const email = formData.get('email');
+            const password = formData.get('password');
+            const response = await fetch(`${API_BASE}/users/signin`, {
+                  method: 'POST',    
+                  headers: { 'Content-Type': 'application/json' },
+                  body: JSON.stringify({ email, password }),
+            });
 
-/*
-try {
-        console.log('button clicked');
-        e.preventDefault();
-        btn.disabled = true;
-        const formData = new FormData(form);
-        await fetch(`${API_BASE}/users/signin`, { method: 'POST', body: formData });
-        alert('로그인 성공');
-        location.href = 'auth/signin.html';
-  } catch (err) {
-        alert('회원가입 실패: ' + err.message);
-  } finally {
-        btn.disabled = false;
-  }*/
+            const data = await response.json();
+            if (!response.ok) {
+                  console.error(data);
+                  throw new Error(`비밀번호 또는 이메일이 올바르지 않습니다.`);
+            } else {
+                  alert('로그인되었습니다.');
+                  // sessionStorage.setItem('token', data.token);
+                  // sessionStorage.setItem('email', email);
+                  // sessionStorage.setItem('id', data.id);
+            }
+            
+            location.href = 'auth/signin.html';
+      } catch (err) {
+            alert('회원가입 실패: ' + err.message);
+      } finally {
+            btn.disabled = false;
+      }
+});
