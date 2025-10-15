ee5824df2bb3764cf55e1c8b5aa95e832cf39db8
Jimin Kim <0305jimin@gmail.com>
2025-10-15 16:20:55 +0900

    chore: moving app.js location

--- keepit-up/app.js
+++ keepit-up/app.js
@@ -2,10 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static('keepit-up')); // 'keepit-up' 폴더를 정적 파일 제공 폴더로 설정
-
 app.get('/', (req, res) => {
-    res.redirect('/auth/signin.html'); // 기본 경로로 접속 시 signin.html로 리다이렉트
+    res.redirect('/auth/signin.html'); // 기본 경로로 접속 시 /auth/signin.html로 리다이렉트
 });
 
 app.listen(port, () => {
