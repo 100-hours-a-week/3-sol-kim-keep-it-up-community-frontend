747fcbaf2e7080f735752165149b98691824269a
Jimin Kim <0305jimin@gmail.com>
2025-10-20 19:28:03 +0900

    fix: resolving an issue that a server cannot find the existing file

--- keepit-up/app.js
+++ keepit-up/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname));
+
 app.get('/', (req, res) => {
     res.redirect('/auth/signin.html'); // 기본 경로로 접속 시 /auth/signin.html로 리다이렉트
 });
