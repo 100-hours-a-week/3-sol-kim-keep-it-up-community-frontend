b608671a0bb7b12c12b674b945eb23f671c407cf
Jimin Kim <0305jimin@gmail.com>
2025-10-22 20:28:00 +0900

    fix: resolving an inssue that duplicate posts have been fetched

--- keepit-up/assets/js/post_list.js
+++ keepit-up/assets/js/post_list.js
@@ -19,63 +19,78 @@ postCreateButton.addEventListener('click', () => {
     게시물 목록 API
 */
 
-let cursorId = 0;
+let cursorId = null;
+let isLoading = false; 
 
 async function fetchPostList() {
+    // 호출 후 마무리가 되기까지는 추가 호출 막기
+    if (isLoading) return;       
+    isLoading = true; 
     const size = 5;
 
     let response = null;
-    if (cursorId == undefined) return;
-    if (cursorId == 0) {
-        response = await fetch(`${API_BASE}/posts?size=${size}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                // 'Authorization': `Bearer ${sessionStorage.getItem('token')}`
-            },
-        });
-        
-    } else {
-        response = await fetch(`${API_BASE}/posts?cursorId=${cursorId}&size=${size}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                // 'Authorization': `Bearer ${sessionStorage.getItem('token')}`
-            },
-        });
-    }
+    // if (cursorId == undefined) return;
 
-    const response_json = await response.json();
-    console.log(response_json);
+    try {
+        if (cursorId == null) {
+            response = await fetch(`${API_BASE}/posts?size=${size}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // 'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                },
+            });
+            // isLoading = true; 
+        } else {
+            response = await fetch(`${API_BASE}/posts?cursorId=${cursorId}&size=${size}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    // 'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+                },
+            });
+            // isLoading = true; 
+        }
 
-    const cursor = response_json.data.content[size - 1];
-    console.log("cursor", cursor);
+        const response_json = await response.json();
+        console.log(response_json);
+        const response_size = response_json.data.content.length;
+        console.log("response_size", response_size);
+        const cursor = response_json.data.content[response_size - 1];
+        const hasNext = !response_json.data.pageable.last;
+        console.log("cursor", cursor);
+        console.log('hasNext', hasNext);
 
-    cursorId = cursor.id;
-    console.log("cursor.id", cursor.id)
+        cursorId = cursor.id;
+        console.log("cursor.id", cursor.id)
 
-    response_json.data.content.forEach(post => {
-        postList.innerHTML += `
-        <div class = "post-card">
-            <div class = "flex-container column" id = "${post.id}">
-                <h2 class = "post-title"><b>${post.title}</b></h2>
-                <div class = "post-info flex-container justify-between">
-                    <div>
-                        <span>좋아요</span> <span>${post.likesCount}</span>
-                        <span>댓글</span> <span>${post.commentsCount}</span>
-                        <span>조회수</span> <span>${post.viewsCount}</span>
+        response_json.data.content.forEach(post => {
+            postList.innerHTML += `
+            <div class = "post-card">
+                <div class = "flex-container column" id = "${post.id}">
+                    <h2 class = "post-title"><b>${post.title}</b></h2>
+                    <div class = "post-info flex-container justify-between">
+                        <div>
+                            <span>좋아요</span> <span>${post.likesCount}</span>
+                            <span>댓글</span> <span>${post.commentsCount}</span>
+                            <span>조회수</span> <span>${post.viewsCount}</span>
+                        </div>
+                        <span>${post.createdAt}</span>
                     </div>
-                    <span>${post.createdAt}</span>
+                </div>
+                <hr/>
+                <div class = "writer-info flex-container justify-start align-center">
+                    <img src="${post.writer.imageUrl}" />
+                    <h3>${post.writer.nickname}</h3>
                 </div>
             </div>
-            <hr/>
-            <div class = "writer-info flex-container justify-start align-center">
-                <img src="${post.writer.imageUrl}" />
-                <h3>${post.writer.nickname}</h3>
-            </div>
-        </div>
-    `;
-    });
+        `;
+        });
+        } catch (e) {
+        console.error(e);
+    } finally {
+        isLoading = false;
+    }
 }
 
 window.onscroll = function(e) {
