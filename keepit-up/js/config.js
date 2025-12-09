eafcdfe49651170ff29fc9f490cc731c31dd795e
Jimin Kim <0305jimin@gmail.com>
2025-12-09 14:06:13 +0900

    chore: using https instead of http

--- keepit-up/js/config.js
+++ keepit-up/js/config.js
@@ -1,3 +1,3 @@
-export const API_BASE = "http://keepit-up.site/api"; // "http://127.0.0.1:8080/api";
+export const API_BASE = "https://keepit-up.site/api"; // "http://127.0.0.1:8080/api";
 export const DEFAULT_IMAGE_PATH = '/assets/images/default_profile_image.png';
 export const GET_PRESIGNED_URL = "https://u27znq8ulf.execute-api.ap-northeast-2.amazonaws.com/getPresignedUrl/image";
